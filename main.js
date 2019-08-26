10a338cd5707403dacab68e3f92637490e667b11
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-08-26 14:23:17 -0400

    cleanup

--- main.js
+++ main.js
@@ -36,17 +36,17 @@ class Blockchain {
 	isChainValid() {
 		for (let i = 1; i < this.chain.length; i++) {
 			const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
-            
-            if (currentBlock.hash !== currentBlock.calculateHash()) {
-                return false;
-            }
+			const previousBlock = this.chain[i - 1];
 
-            if (currentBlock.previousHash !== previousBlock.hash) {
-                return false;
-            }
+			if (currentBlock.hash !== currentBlock.calculateHash()) {
+				return false;
+			}
 
-            return true;
+			if (currentBlock.previousHash !== previousBlock.hash) {
+				return false;
+			}
+
+			return true;
 		}
 	}
 }
