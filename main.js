06225a6212468dc080cd25524d457a3da4c982c8
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-08-26 00:54:33 -0400

    blockchain class complete

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
-// Package provides cryptographic hash functions
 const SHA256 = require('crypto-js/sha256');
 
 class Block {
 	constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = previousHash;
-        this.hash = this.calculateHash();
-    }
+		this.index = index;
+		this.timestamp = timestamp;
+		this.data = data;
+		this.previousHash = previousHash;
+		this.hash = this.calculateHash();
+	}
 
-    // Returns SHA256 hash as a string
-    calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
-    }
+	calculateHash() {
+		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+	}
+}
+
+class Blockchain {
+	constructor() {
+		this.chain = [];
+	}
+
+	createGenesisBlock() {
+		return new Block(0, '03/14/1989', 'Genesis Block', '3141');
+	}
+
+	getLatestBlock() {
+		return this.chain[this.chain.length - 1];
+	}
+
+	addBlock(newBlock) {
+		newBlock.previousHash = this.getLatestBlock().hash;
+		newBlock.hash = newBlock.calculateHash();
+		this.chain.push(newBlock);
+	}
 }
