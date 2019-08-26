82f6ece5e4c9dfe1de259ac90491c48e19312c38
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-08-26 00:29:19 -0400

    block class complete

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+// Package provides cryptographic hash functions
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
 	constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash();
+    }
+
+    // Returns SHA256 hash as a string
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
