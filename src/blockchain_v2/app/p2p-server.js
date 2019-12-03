3b38fe6956964426cc9344bbdf780ea7e6b39bb7
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-12-02 20:37:57 -0500

    Finished P2P class skeleton and listen method

--- src/blockchain_v2/app/p2p-server.js
+++ src/blockchain_v2/app/p2p-server.js
@@ -1,5 +1,5 @@
 // Import Dependencies
-const ws = require('ws');
+const WebSocket = require('ws');
 
 // Require Dot ENV
 require('dotenv').config();
@@ -10,3 +10,29 @@ const P2P_PORT = process.env.P2P_PORT || 5001;
 // Set list of Peers
 const peers = process.env.PEERS ? process.env.PPERS.split(',') : [];
 
+// P2P Server
+class P2Pserver {
+    constructor(blockchain) {
+        this.blockchain = blockchain;
+        this.sockets = [];
+    }
+
+    // Instantiate new P2P server and connections
+    listen() {
+        const server = new WebSocket.Server({ port: P2P_PORT });
+
+        server.on('connect', socket => this.connectSocket(socket));
+
+        this.connectToPeers();
+
+        console.log(`Listening for peer to peer connection on port : ${P2P_PORT}`)
+    }
+
+    connectSocket(socket) {
+
+    }
+
+    connectToPeers() {
+
+    }
+}
